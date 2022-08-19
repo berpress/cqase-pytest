9ee2b381af4e5b90dc7e862a3ca59962a469ce48
alexanderlozovoy <berpress@gmail.com>
2022-08-19 16:55:24 +0300

    Add filelock

--- pytest_cqase/plugin.py
+++ pytest_cqase/plugin.py
@@ -1,9 +1,14 @@
+import logging
+import pathlib
+
 import pytest
 from cqase.client import QaseClient
+from filelock import FileLock
 
 # start wright plugin
 from pytest_cqase.singltone_like import QaseObject
 
+logger = logging.getLogger("qase")
 
 class qase:
     """Class with decorators for pytest"""
@@ -14,19 +19,27 @@ class qase:
         """
         return pytest.mark.qase(ids=ids)
 
+
 class PyTestQasePlugin:
     def __init__(self, client: QaseClient):
         self.client = client
         self.qase_object = QaseObject()
+        self.meta_run_file = pathlib.Path("qaseio.runid")
+        self.url = "https://app.qase.io/run/{}/dashboard/{}"
 
     QASE_MARKER = 'qase'
-    # qase_object = QaseObject()
     test_run_id = None
-    COMMENT = "RUN FROM AUTOMATION TESTS"
+    COMMENT = "Pytest Plugin Automation Run"
+    TEST_STATUS = {
+        "PASSED": 'passed',
+        "FAILED": 'failed',
+        "SKIPPED": 'skipped',
+        "BLOCKED": 'blocked',
+    }
 
-    def get_qase_ids(self, items):
+    def _get_qase_ids(self, items) -> list:
         """
-        взять список ids из декоратора ids
+        get qase ids
         """
         testcase_ids = []
         for item in items:
@@ -36,9 +49,25 @@ class PyTestQasePlugin:
                 [testcase_ids.append(id_) for id_ in ids]
         return testcase_ids
 
-    def create_test_run(self, qase_ids: list) -> int:
+    def load_run_from_lock(self):
         """
-        создать тест ран
+        Get test id from file
+        """
+        if self.meta_run_file.exists():
+            with open(self.meta_run_file, "r") as lock_file:
+                try:
+                    test_run_id = int(lock_file.read())
+                    return test_run_id
+                except ValueError:
+                    pass
+
+    def _create_file_lock(self, test_run_id: int):
+        with open(self.meta_run_file, "w") as lock_file:
+            lock_file.write(str(test_run_id))
+
+    def _create_test_run(self, qase_ids: list) -> int:
+        """
+        create qase test run
         """
         body = {"title": "test run", "cases": qase_ids}
         res = self.client.runs.create(code="TP", body=body)
@@ -47,67 +76,57 @@ class PyTestQasePlugin:
     def create_qase_results_object(self, testcase_ids):
         self.qase_object.create_test_ids_dict(testcase_ids)
 
-    def add_test_result(self):
-        pass
-
     @pytest.hookimpl(trylast=True)
-    def pytest_collection_modifyitems(self, session, config, items):
-        qase_ids: list = self.get_qase_ids(items)
-        self.create_qase_results_object(qase_ids)
-        test_run_id = self.create_test_run(qase_ids)
-        QaseObject().test_run_id = test_run_id
-        pass
+    def pytest_collection_modifyitems(self, items):
+        with FileLock("qaseio.lock"):
+            qase_ids: list = self._get_qase_ids(items)
+            self.create_qase_results_object(qase_ids)
+            test_run_id = self.load_run_from_lock()
+            if test_run_id is None:
+                test_run_id = self._create_test_run(qase_ids)
+                self._create_file_lock(test_run_id)
+            QaseObject().test_run_id = test_run_id
+            logger.info(f"Create new test run: {self.url.format('TP', test_run_id)}")
 
-    def get_qase_id_from_report(self, item):
-        # возможно несколько тест кейсов
+    def _get_qase_id_from_report(self, item) -> int:
+        # TODO may be same ids
         marks = item.own_markers
-        qase_mark = list(mark for mark in marks if mark.name == 'qase')
+        qase_mark = list(mark for mark in marks if mark.name == self.QASE_MARKER)
         if len(qase_mark) > 0:
             return qase_mark[0].kwargs.get('ids')[0]
-        return None
-
-    results = {}
+        return -1
 
     @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_makereport(self, item, call):
         outcome = yield
         report = outcome.get_result()
-        # docstrings
         test_fn = item.obj
         if report.when != 'teardown':
-            docstring = getattr(test_fn, '__doc__')
-            qase_id = self.get_qase_id_from_report(item)
-            # qase_object.test_cases[qase_id].stacktrace = "".join([docstring, report.caplog])
+            description = self.COMMENT
+            docstring = getattr(test_fn, '__doc__', None)
+            qase_id = self._get_qase_id_from_report(item)
             result = report.outcome
             duration = report.duration
-            if docstring:
-                docstring = "".join([docstring, report.caplog])
-            if report.outcome == 'failed':
-                if qase_id:
+            if qase_id:
+                if docstring:
+                    description = "".join([self.COMMENT, docstring, report.caplog])
+                self.qase_object.test_cases[qase_id].description = description
+                self.qase_object.test_cases[qase_id].duration = duration
+                if report.outcome == self.TEST_STATUS.get('FAILED'):
+                    self.qase_object.test_cases[qase_id].result = result
                     stacktrace = report.longreprtext
-                    self.qase_object.test_cases[qase_id].result = result
                     self.qase_object.test_cases[qase_id].stacktrace = stacktrace
-                    self.qase_object.test_cases[qase_id].description = docstring
-                    self.qase_object.test_cases[qase_id].duration = duration
                     return
-            if report.outcome == 'skipped' and report.longrepr:
-                if qase_id:
+                if report.outcome == self.TEST_STATUS.get(
+                        'SKIPPED') and report.longrepr:
                     self.qase_object.test_cases[qase_id].result = result
-                    self.qase_object.test_cases[qase_id].comment = docstring
-                    self.qase_object.test_cases[qase_id].duration = duration
                     return
-            if report.outcome == 'skipped':
-                if qase_id:
+                if report.outcome == self.TEST_STATUS.get('SKIPPED'):
                     self.qase_object.test_cases[qase_id].result = result
-                    self.qase_object.test_cases[qase_id].comment = docstring
-                    self.qase_object.test_cases[qase_id].duration = duration
                     return
-            if report.outcome == 'passed':
-                if qase_id:
+                if report.outcome == self.TEST_STATUS.get('PASSED'):
                     if self.qase_object.test_cases[qase_id].result != 'failed':
                         self.qase_object.test_cases[qase_id].result = result
-                    self.qase_object.test_cases[qase_id].description = docstring
-                    self.qase_object.test_cases[qase_id].duration = duration
                     return
 
     def _create_bulk_body(self):
@@ -117,18 +136,16 @@ class PyTestQasePlugin:
                 if value.result != 'untested':
                     results_send.append(
                         {"case_id": value.qase_id, "status": value.result,
-                         "comment": self.COMMENT, 'stacktrace': value.stacktrace,
+                         "comment": value.description,
+                         'stacktrace': value.stacktrace,
                          "time_ms": int(value.duration * 1000),
-                         "case": {
-                             "description": value.description
-                         }})
+                         })
             return {'results': results_send}
         except AttributeError:
             pass
-        pass
 
     def pytest_sessionfinish(self, session, exitstatus):
         body = self._create_bulk_body()
         res_bulk = self.client.results.bulk(code="TP", uuid=QaseObject().test_run_id,
-                                       body=body)
+                                            body=body)
         assert res_bulk
